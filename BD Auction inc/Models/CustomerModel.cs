b9b382bd7b7ab2c55095184031e560018c43fcc4
De Xian <ahx.agent007@gmail.com>
2019-02-24 00:47:06 +0600

    Working on Databse Connection

---
+++ BD Auction inc/Models/CustomerModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BD_Auction_inc.Models
+{
+    public class CustomerModel{
+
+        private int cID { get; set; }
+        private string cNumber { get; set; }
+        private string cName { get; set; }
+        private string cAddress { get; set; }
+        private string cEmail { get; set; }
+        private string cNID { get; set; }
+        private int cRating { get; set; }
+        private int BidLimit { get; set; }
+        private string VarificationStatus { get; set; }
+
+    }
+}
